3cb827453becfe5502383eec36815c40dde75075
William Colin <william.colin@gmail.com>
2019-02-01 11:12:59 -0600

    -

--- modulo_trova_isr/trova_isr/__manifest__.py
+++ modulo_trova_isr/trova_isr/__manifest__.py
@@ -3,14 +3,13 @@
     'name': "trova_isr",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        """,
 
     'description': """
-        Long description of module's purpose
+      
     """,
 
-    'author': "My Company",
+    'author': "xmarts",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
